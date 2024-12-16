524a8bd550f1452a8028d53fa9f4abdcb6cfdafb
Ford Henley <info@fordswebhelp.com>
2024-12-16 12:07:01 -0600

    Initial commit

---
+++ demo/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Demo</title>
+  </head>
+  <body>
+    <h1>
+      <?php
+       $greeting = "Hello";
+        echo "$greeting World";
+      ?>
+    </h1>
+  </body>
+</html>
