4f4a584dfdb4e33b2757a9fa266919f46ad8f92b
Ford Henley <info@fordswebhelp.com>
2024-12-20 11:21:57 -0600

    completed conditionals and booleans lesson

--- demo/index.php
+++ demo/index.php
@@ -3,13 +3,29 @@
   <head>
     <meta charset="UTF-8" />
     <title>Demo</title>
+    <style>
+      body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+      }
+    </style>
   </head>
   <body>
+    <?php 
+      $name = "Dark Matter";
+      $read = true;
+
+      if($read){
+        $message = "You have read $name";
+      } else {
+        $message = "You have NOT read $name";
+      }
+    ?>
     <h1>
-      <?php
-       $greeting = "Hello";
-        echo "$greeting World";
-      ?>
+      <?= $message ?>
     </h1>
   </body>
 </html>
