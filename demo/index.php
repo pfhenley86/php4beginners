65c392360c6b76a3a25eb2153f5ffa468a35c6c7
Ford Henley <info@fordswebhelp.com>
2025-01-10 14:53:03 -0600

    Completed 'Separate Logic From the Template'

--- demo/index.php
+++ demo/index.php
@@ -1,49 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Demo</title>
-  </head>
-  <body>
-    <h1>Recommended Books</h1>
+<?php
 
-    <?php
-      $books = [
-        [
-          'name' => 'Do Androids Dream of Electric Sheep',
-          'author' => 'Philip K. Dick',
-          'releaseYear' => 1968,
-          'purchaseUrl' => 'http://example.com'
-        ],
-        [
-          'name' => 'Project Hail Mary',
-          'author' => 'Andy Weir',
-          'releaseYear' => 2021,
-          'purchaseUrl' => 'http://example.com'
-        ],
-        [
-          'name' => 'The Martian',
-          'author' => 'Andy Weir',
-          'releaseYear' => 2011,
-          'purchaseUrl' => 'http://example.com'
-        ]
-      ];
+$books = [
+  [
+    'name' => 'Do Androids Dream of Electric Sheep',
+    'author' => 'Philip K. Dick',
+    'releaseYear' => 1968,
+    'purchaseUrl' => 'http://example.com'
+  ],
+  [
+    'name' => 'Project Hail Mary',
+    'author' => 'Andy Weir',
+    'releaseYear' => 2021,
+    'purchaseUrl' => 'http://example.com'
+  ],
+  [
+    'name' => 'The Martian',
+    'author' => 'Andy Weir',
+    'releaseYear' => 2011,
+    'purchaseUrl' => 'http://example.com'
+  ]
+];
 
-    
 
-      $filteredBooks = array_filter($books, function($book){
-        return $book['author'] === 'Andy Weir';
-      });
 
-    ?>
-    <ul>
-      <?php foreach ($filteredBooks as $book) :?>
-        <li>
-          <a href="<?= $book['purchaseUrl'];?>">
-            <?= $book['name'];?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
-          </a>
-        </li>
-      <?php endforeach;?>
-    </ul>
-  </body>
-</html>
+$filteredBooks = array_filter($books, function($book){
+  return $book['author'] === 'Andy Weir';
+});
+
+require "index.view.php";
